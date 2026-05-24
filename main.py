a8ce42b228890c4b6c5a5ca133f3b6758f69bd99
sundaram123krishnan <krishnsundaram@gmail.com>
2026-05-24 20:56:24 +0530

    Test the graph construction

--- main.py
+++ main.py
@@ -1,29 +1,6 @@
-from dataclasses import dataclass
-
-from dependency_graph import DependencyGraph, build_graph, resolve_graph
 from api import fetch_package_metadata
-
-
-@dataclass(frozen=True)
-class BuildTarget:
-    provider: "pypi.org" # hardcode it for now, later we can support other providers
-    name: str
-    version: str | None = None
-
-
-# only contains non-optional dependencies
-@dataclass(frozen=True)
-class DependencySpec:
-    name: str
-    requirement: str
-
-
-@dataclass(frozen=True)
-class PackageMetadata:
-    provider: "pypi.org"
-    name: str
-    version: str
-    dependencies: tuple[DependencySpec, ...]
+from dependency_graph import DependencyGraph, build_graph, resolve_graph
+from models import BuildTarget
 
 
 class Coprtree:
@@ -38,5 +15,5 @@ class Coprtree:
 
 
 if __name__ == "__main__":
-    coprtree = Coprtree(BuildTarget(provider="pypi.org", name="django"))
-    print(coprtree.metadata)
+    coprtree = Coprtree(BuildTarget(provider="pypi.org", name="flask"))
+    print(coprtree.graph._edges)
