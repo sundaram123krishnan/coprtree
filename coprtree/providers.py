2d581573d43166eb0e48b8687bd3831a5a96c532
sundaram123krishnan <krishnsundaram@gmail.com>
2026-06-23 20:36:53 +0530

    Define version constraints

--- coprtree/providers.py
+++ coprtree/providers.py
@@ -1,32 +1,30 @@
-from functools import partial
-import re
-
 from .exceptions import UnsupportedProvider
 from .models import Provider
-
-
-def _provide(wrapper: str, name: str) -> str:
-    return f"{wrapper}({name})"
-
-
-def _perl_provide(name: str) -> str:
-    return "perl" if name == "perl" else _provide("perl", name.replace("-", "::"))
+from .normalize import (
+    cpan_constraints,
+    cpan_name,
+    cpan_provide,
+    pypi_constraints,
+    pypi_name,
+    pypi_provide,
+)
 
 
 PYPI = Provider(
     registry="pypi.org",
     dep_kinds=frozenset({"runtime"}),
-    # pep 503 normalized names
-    normalize=lambda n: re.sub(r"[-_.]+", "-", n.split("[", 1)[0]).lower(),
-    fedora_provide=partial(_provide, "python3dist"),
+    normalize=pypi_name,
+    fedora_provide=pypi_provide,
+    version_constraints=pypi_constraints,
 )
 
 
 CPAN = Provider(
     registry="metacpan.org",
     dep_kinds=frozenset({"configure", "build", "test", "runtime"}),
-    normalize=lambda n: n,
-    fedora_provide=_perl_provide,
+    normalize=cpan_name,
+    fedora_provide=cpan_provide,
+    version_constraints=cpan_constraints,
 )
 
 
