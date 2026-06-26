ed277a71ebbfe6552161bb2fec56225cbacac4bf
sundaram123krishnan <krishnsundaram@gmail.com>
2026-06-26 17:46:12 +0530

    Use the chrootspec's repos

--- coprtree/repo_check.py
+++ coprtree/repo_check.py
@@ -6,7 +6,8 @@ from collections.abc import Callable
 import dnf
 import hawkey
 
-from .constants import COPR_BASEURL, FEDORA_METALINK, UPDATES_METALINK
+from .chroots import get_distribution, parse_chroot
+from .constants import COPR_BASEURL
 from .models import BuildEnv, Provider
 
 
@@ -16,14 +17,15 @@ def _repo_adder(base: dnf.Base) -> Callable[..., dnf.repo.Repo]:
 
 @functools.cache
 def _sack(env: BuildEnv) -> dnf.sack.Sack:
-    release, arch = _release_arch(env.chroot)
+    distro, release, arch = parse_chroot(env.chroot)
+    chroot = get_distribution(distro)
     base = dnf.Base()
     base.conf.substitutions["releasever"] = release
     base.conf.substitutions["basearch"] = arch
     base.conf.substitutions["arch"] = arch
     add_repo = _repo_adder(base)
-    add_repo("fedora", metalink=FEDORA_METALINK.format(release=release, arch=arch))
-    add_repo("updates", metalink=UPDATES_METALINK.format(release=release, arch=arch))
+    for repo_id, kwargs in chroot.repos(release, arch):
+        add_repo(repo_id, **kwargs)
     add_repo(
         "copr",
         baseurl=[COPR_BASEURL.format(project=env.copr_project, chroot=env.chroot)],
@@ -50,16 +52,10 @@ def has_package_in_repository(
         return False
 
     sack = _sack(env)
-    capability = provider.fedora_provide(name)
+    capability = provider.provide(name)
     query = sack.query().available()
     if constraints:
         query = _check_package_version(sack, query, capability, constraints)
     else:
         query = query.filter(provides=capability)
     return bool(query.run())
-
-
-def _release_arch(chroot: str) -> tuple[str, str]:
-    # fedora-44-x86_64 -> ("44", "x86_64"); rawhides not handled
-    _, release, arch = chroot.split("-", 2)
-    return release, arch
